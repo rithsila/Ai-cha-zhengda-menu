3ee16a3090a1b060820171f2f453aeb93a299199
rith sila <khmertools7@gmail.com>
2026-09-15 16:21:39 +0700

    fix: remove better-sqlite3 to fix Railway build

--- apps/api/src/run-migration.ts
+++
@@ -1,9 +0,0 @@
-
-import { migrateDataFromSqlite } from "./migrate-data";
-migrateDataFromSqlite("./production.db")
-  .then(() => process.exit(0))
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
-
